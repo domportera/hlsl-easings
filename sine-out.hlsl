b394cea5a7f1f53036a5f29b1ea7d85713411a28
Dom <domportera@gmail.com>
2022-12-14 20:49:10 -0500

    doing that again

--- sine-out.hlsl
+++ sine-out.hlsl
@@ -1,9 +1,10 @@
+#ifndef SINE_OUT
+#define SINE_OUT
+
 #ifndef HALF_PI
 #define HALF_PI 1.5707963267948966
 #endif
 
-#ifndef SINE_OUT
-#define SINE_OUT
 float sineOut(float t) {
   return sin(t * HALF_PI);
 }
